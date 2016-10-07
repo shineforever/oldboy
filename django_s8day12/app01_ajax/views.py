67eadc0949f346183d065383a1b900d06ac54bd8
shineforever <shine_forever@yeah.net>
2016-10-07 20:28:07 +0800

    ajax jQuery 解析json格式，获得指定的字段

--- django_s8day12/app01_ajax/views.py
+++ django_s8day12/app01_ajax/views.py
@@ -2,13 +2,15 @@
 # coding:utf-8
 
 from django.shortcuts import render,render_to_response,HttpResponse
-# from django.http.response import HttpResponse
+import json
 
 # Create your views here.
 
 def index(request):
     if request.method == 'POST':
         print request.POST
-        return HttpResponse("msg from ajax server")
+        data = {'status':0,'msg':'请求成功','data':[111,3333,5555]}
+        return HttpResponse(json.dumps(data))
+        # return HttpResponse("msg from ajax server")
     else:
         return render_to_response('app01/ajax.html')
